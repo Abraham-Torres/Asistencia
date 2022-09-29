a33a9ccc7c45e60353d9adc261fdffa8b59fc3a6
Abraham-Torres <abraham_torres81@outlook.com>
2022-09-29 14:15:18 -0500

    mostrar datos de db

---
+++ AVIADMIN-WEB/forms/puesto/Puesto.py
@@ -0,0 +1,19 @@
+class Puesto:
+    
+    def __init__(self, identificador, nombre, correo, edad, tipo_puesto, password):
+        self.identificador = identificador
+        self.nombre = nombre
+        self.correo = correo
+        self.edad = edad
+        self.tipo_puesto = tipo_puesto
+        self.password = password
+    
+    def datoPuestoJson(self):
+        return{
+            "identificador": self.identificador,
+            "nombre": self.nombre,
+            "correo": self.correo,
+            "edad": self.edad,
+            "tipo_puesto": self.tipo_puesto,
+            "password": self.password
+        }    
--- AVIADMIN-WEB/index.py
+++ AVIADMIN-WEB/index.py
@@ -1,14 +1,42 @@
-from flask import Flask,render_template#importamos flask
+from flask import Flask,render_template,request,url_for,redirect#importamos flask
 from data_base import database as mongodb
+from forms.puesto.Puesto import Puesto
+import random
+
 
 DB=mongodb.dbConecction()
 
 app=Flask(__name__)
 
+
+#seccion de puesto
 @app.route('/Registrar_puesto')
 def Registrar_puesto():
     titulo="Nuevo puesto"
     return render_template('administrador/puesto/Registrar_puesto.html', titulo=titulo)
+
+@app.route('/Nuevo-puesto',methods=['POST'])
+def NuevoPuesto():
+   puestos=DB['puestos']#conexion
+   nombre=request.form['nombre']
+   correo=request.form['correo']
+   edad=request.form['edad']
+   tipo_puesto=request.form['tipo_puesto']
+   password=request.form['password']
+   identificador=str(random.randint(0,2000))+correo
+
+   if nombre and correo and edad and tipo_puesto and password:
+        puesto=Puesto(identificador,nombre,correo,edad,tipo_puesto,password) 
+        puestos.insert_one(puesto.datoPuestoJson())
+        return redirect('/Registrar_puesto')    
+
+#mostrar datos de puesto
+@app.route('/db-puesto')
+def base_datos_puesto():  
+    puestos=DB['puestos']
+    titulo="BD puestos"
+    puestosRecibidos=puestos.find()#para buscar en general
+    return render_template('administrador/puesto/base-datos.html',titulo=titulo,puesto=puestosRecibidos)
 
 @app.route('/')#ruta
 def inicio():
