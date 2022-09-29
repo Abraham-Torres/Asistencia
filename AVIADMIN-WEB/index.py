c93db0cd3b02c0d27bd3405bbb179a0533ec402f
Abraham-Torres <abraham_torres81@outlook.com>
2022-09-29 12:21:14 -0500

    formulario puesto

--- AVIADMIN-WEB/index.py
+++ AVIADMIN-WEB/index.py
@@ -1,11 +1,19 @@
-from distutils.log import debug
 from flask import Flask,render_template#importamos flask
+from data_base import database as mongodb
+
+DB=mongodb.dbConecction()
 
 app=Flask(__name__)
 
+@app.route('/Registrar_puesto')
+def Registrar_puesto():
+    titulo="Nuevo puesto"
+    return render_template('administrador/puesto/Registrar_puesto.html', titulo=titulo)
+
 @app.route('/')#ruta
 def inicio():
-    return render_template('inicio.html')#render template agarra cualquier archivo que este en su carpeta
+    titulo="Inicio administrador"
+    return render_template('index.html',titulo=titulo)#render template agarra cualquier archivo que este en su carpeta
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",debug=True)#configuracion del host
